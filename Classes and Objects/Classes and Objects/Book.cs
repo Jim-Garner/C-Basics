d4fc3615e9c95dcb20fbcca8aa15cfa47ed90ce7
Jim-Garner <47324596+Jim-Garner@users.noreply.github.com>
2019-04-23 16:11:50 +0100

    Upload

---
+++ Classes and Objects/Classes and Objects/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_and_Objects
+{
+
+    //This is a class. This is a template created so we can create an object in our main program.
+    class Book
+    {
+        //These are the attributes we've assigned to the book class;
+        public string title;
+        public string author;
+        public int pages;
+
+    }
+}
---
+++ Classes and Objects/Classes and Objects/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_and_Objects
+{
+    class Student
+    {
+        public string name;
+        public int age;
+        public string gender;
+
+    }
+}
